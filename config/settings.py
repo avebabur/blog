bca85e68e8eae887a267a56b4cd99b66362ece35
avebabur <babxodjambergenov123456789@gmail.com>
2023-06-16 19:06:33 +0500

    2023-06-16

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jtczps&@mz#d(x$qgu!rm7zra6)=)!bqj=7b$cvv3le_w+6ng)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     #APPS
-    'blog.apps.BlogConfig'
+    'blog.apps.BlogConfig',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
@@ -78,8 +79,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sertifiedproject',
+	'USER': 'sertifiedprojectuser',
+	'PASSWORD': 'sertifiedprojectuserpassword',
+	'HOST': 'localhost',
+	'PORT': '',
     }
 }
 
@@ -119,8 +124,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'djangotestavebabur@gmail.com'
+EMAIL_HOST_PASSWORD = 'gwmr cfjs uqgv xhjq'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
